db4271bd2c07433230ca16ae34bda9b2a8b5bec4
Sol Lavarello <lavarellosolange@gmail.com>
2018-07-10 10:17:37 -0500

    interfaz de log in y eleccion de sign up

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems);
+
+    instances.start();
+  });
+
+  
