c03e319b7f5ba670a218a07d2bc9244940cda329
Sol Lavarello <lavarellosolange@gmail.com>
2018-07-12 18:35:42 -0500

    segunda prueba de auth google

--- src/main.js
+++ src/main.js
@@ -121,6 +121,7 @@ const googleAccount = () => {
     var token = result.credential.accessToken;
     // The signed-in user info.
     var user = result.user;
+    console.log(user.displayName);
     // ...
   }).catch(function(error) {
     // Handle Errors here.
