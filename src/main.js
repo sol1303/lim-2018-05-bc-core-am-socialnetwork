41ce259658cbdf2a12b7ca2afba14d85c2e28731
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-07-15 22:10:56 -0500

    pases de modals para usuarios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // sections
 const sectionLogIn = document.getElementById("main-log-in");
 const sectionUserSelection = document.getElementById("select-user");
-const sectionSignUpUsers = document.getElementById("main-sign-up-users");
 const sectionSignUpDoctors = document.getElementById("main-sign-up-doctors");
+const sectionSignUpUsers = document.getElementById("main-sign-up-users");
 const sectionResponseLogIn = document.getElementById("response-log-in");
 const sectionResponseSignUp = document.getElementById("response-sign-up");
 const sectionLogOut = document.getElementById("log-out");
@@ -18,6 +18,12 @@ const btnFacebookLogIn = document.getElementById("btn-fb-log-in");
 // inputs
 const txtEmailLogIn = document.getElementById("txt-user-mail-login");
 const txtPasswordLogIn = document.getElementById("txt-user-password-login");
+const txtEspecialidad = document.getElementById("specialty");
+const txtColegiatura = document.getElementById("colegiatura");
+const txtDoctorNameSignUp = document.getElementById("txt-doctor-name-signup");
+const txtDoctorEmailSignUp = document.getElementById("txt-doctor-mail-signup");
+const txtDoctorPasswordSignUp = document.getElementById("txt-doctor-password-signup");
+const txtDoctorConfirmPasswordSignUp = document.getElementById("txt-doctor-confirm-password-signup");
 const txtUserNameSignUp = document.getElementById("txt-user-name-signup");
 const txtUserEmailSignUp = document.getElementById("txt-user-mail-signup");
 const txtUserPasswordSignUp = document.getElementById("txt-user-password-signup");
@@ -27,8 +33,8 @@ const txtUserConfirmPasswordSignUp = document.getElementById("txt-user-confirm-p
 const goToSignUp = document.getElementById("go-to-sign-up");
 const goToLogInFromDoctors = document.getElementById("go-to-log-in-doctors");
 const goToLogInFromUsers = document.getElementById("go-to-log-in-users");
-const goToSignUpUsers = document.getElementById("sign-up-selection-users");
 const goToSignUpDoctors = document.getElementById("sign-up-selection-doctors");
+const goToSignUpUsers = document.getElementById("sign-up-selection-users");
 
 // modals
 const modalDoctors = document.getElementById('modal-doctors');
@@ -37,27 +43,34 @@ const closeModalDoctors = document.getElementsByClassName("close")[0];
 const closeModalUsers = document.getElementsByClassName("close")[1];
 
 // modals buttons
-const btnModalSignUpUsers = document.getElementById("btn-email-modal-sign-up-users");
-const btnModalFbSignUpUsers = document.getElementById("btn-fb-modal-sign-up-users");
-const btnModalGgSignUpUsers = document.getElementById("btn-gg-modal-sign-up-users");
 const btnModalSignUpDoctors = document.getElementById("btn-email-modal-sign-up-doctors");
 const btnModalFbSignUpDoctors = document.getElementById("btn-fb-modal-sign-up-doctors");
 const btnModalGgSignUpDoctors = document.getElementById("btn-gg-modal-sign-up-doctors");
+const btnModalSignUpUsers = document.getElementById("btn-email-modal-sign-up-users");
+const btnModalFbSignUpUsers = document.getElementById("btn-fb-modal-sign-up-users");
+const btnModalGgSignUpUsers = document.getElementById("btn-gg-modal-sign-up-users");
 
 // labels helpers
 const helperEmailLogIn = document.getElementById("incorrect-email");
 const helperPasswordLogIn = document.getElementById("incorrect-password");
-const helperNameUserSignUp = document.getElementById("incorrect-name-sign-up-users");
-const helperEmailUserSignUp = document.getElementById("incorrect-email-sign-up-users");
-const helperPasswordUserSignUp = document.getElementById("incorrect-password-sign-up-users");
-const helperConfirmPasswordUserSignUp = document.getElementById("incorrect-confirm-password-sign-up-users");
+const helperEspecialidad = document.getElementById("incorrect-specialty");
+const helperColegiatura = document.getElementById("incorrect-colegiatura");
+const helperNameDoctorSignUp = document.getElementById("incorrect-doctor-name-sign-up");
+const helperEmailDoctorSignUp = document.getElementById("incorrect-doctor-mail-sign-up");
+const helperPasswordDoctorSignUp = document.getElementById("incorrect-doctor-password-sign-up");
+const helperConfirmPasswordDoctorSignUp = document.getElementById("incorrect-doctor-confirm-password-sign-up");
+const helperNameUserSignUp = document.getElementById("incorrect-user-name-sign-up");
+const helperEmailUserSignUp = document.getElementById("incorrect-user-email-sign-up");
+const helperPasswordUserSignUp = document.getElementById("incorrect-user-password-sign-up");
+const helperConfirmPasswordUserSignUp = document.getElementById("incorrect-user-confirm-password-sign-up");
 
 const patronEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 
 const user = {
   name: '',
   email: '',
-  password: ''
+  password: '',
+  type: ''
 }
 
 const config = {
@@ -77,7 +90,9 @@ window.onload = () => {
       sectionLogIn.hidden = true;
       sectionResponseLogIn.hidden = false;
       sectionLogOut.hidden = false;
-      console.log("usuario logueado")
+      sectionUserSelection.hidden = true;
+      modalUsers.style.display = "none";
+      modalDoctors.style.display = "none";
       // User is signed in.
       // const displayName = user.displayName;
       // const email = user.email;
@@ -85,7 +100,6 @@ window.onload = () => {
       // const photoURL = user.photoURL;
       // const uid = user.uid;
     } else {
-      console.log("no estas logueado");
       M.updateTextFields();
     }
   });
@@ -95,18 +109,28 @@ const logOut = () => {
   firebase.auth().signOut().then(() => {
     txtEmailLogIn.value = "";
     txtPasswordLogIn.value = "";
+    // doctores
+    txtEspecialidad.value = "";
+    txtColegiatura.value = "";
+    txtDoctorNameSignUp.value = "";
+    txtDoctorEmailSignUp.value = "";
+    txtDoctorPasswordSignUp.value = "";
+    txtDoctorConfirmPasswordSignUp.value = "";
+    //pacientes
     txtUserNameSignUp.value = "";
     txtUserEmailSignUp.value = "";
     txtUserPasswordSignUp.value = "";
     txtUserConfirmPasswordSignUp.value = "";
+
     sectionLogOut.hidden = true;
     sectionResponseLogIn.hidden = true;
     sectionUserSelection.hidden = true;
     sectionResponseSignUp.hidden = true;
+    // formularios de email
     sectionSignUpUsers.hidden = true;
     sectionSignUpDoctors.hidden = true;
+    // cuando se salga de sesión para cualquier caso siempre se mostrará el login
     sectionLogIn.hidden = false;
-    console.log("saliste");
   });
 }
 
@@ -136,26 +160,79 @@ const validateLogIn = () => {
   }
 }
 
+const showMuro = () => {
+  document.getElementById("user-name-sign-up").innerHTML = user.name;
+  sectionSignUpDoctors.hidden = true;
+  sectionSignUpUsers.hidden = true;
+  sectionResponseSignUp.hidden = false;
+  sectionLogOut.hidden = false;
+}
+
+const signUpByDoctors = () => {
+  const auth = firebase.auth();
+  const promise = auth.createUserWithEmailAndPassword(user.email, user.password).then(() => {
+    const x = firebase.auth().currentUser;
+    if (x) {
+      x.sendEmailVerification().then(() => {
+        console.log("se envió correo de verificación de cuenta al correo");
+        showMuro();
+      }).catch(function (error) {
+        alert(error);
+      });
+    }
+  });
+  promise.catch(e => alert(e.message));
+}
+
+//validaciones de signup divisiones para users
+const ableSignUpByDoctors = () => {
+  let name, email, password, confirmPassword;
+  //nombre
+  if (txtDoctorNameSignUp.value.length > 0) {
+    name = true;
+  } else if (!txtDoctorNameSignUp.value.length > 0) {
+    name = false;
+  }
+  //email
+  if (txtDoctorEmailSignUp.value.length > 0 && patronEmail.test(txtDoctorEmailSignUp.value)) {
+    email = true;
+  } else if (txtDoctorEmailSignUp.value.length === 0 || !patronEmail.test(txtDoctorEmailSignUp.value)) {
+    email = false;
+  }
+  //new password
+  if (txtDoctorPasswordSignUp.value.length >= 6) {
+    password = true;
+  } else if (txtDoctorPasswordSignUp.value.length < 6) {
+    password = false;
+  }
+  //confirm password
+  if (txtDoctorConfirmPasswordSignUp.value.length >= 6 && txtDoctorConfirmPasswordSignUp.value === txtDoctorPasswordSignUp.value) {
+    confirmPassword = true;
+  } else if (txtDoctorConfirmPasswordSignUp.value.length < 6 || txtDoctorConfirmPasswordSignUp.value !== txtDoctorPasswordSignUp.value) {
+    confirmPassword = false;
+  }
+  // si todas las etiquetas estan ocultas hará el registro
+  if (name && email && password && confirmPassword) {
+    user.name = txtDoctorNameSignUp.value;
+    user.email = txtDoctorEmailSignUp.value;
+    user.password = txtDoctorPasswordSignUp.value;
+    signUpByDoctors();
+  }
+}
+
 const signUpUsers = (e) => {
   if (e.currentTarget.id === "btn-email-modal-sign-up-users") {
     sectionSignUpUsers.hidden = false;
     sectionUserSelection.hidden = true;
     modalUsers.style.display = "none";
   } else {
-    console.log(e.currentTarget.id);
+    txtEspecialidad.value = "";
+    txtColegiatura.value = "";
     // aqui se deberia mostrar el signup para los doctores
     sectionSignUpDoctors.hidden = false;
     sectionUserSelection.hidden = true;
     modalDoctors.style.display = "none";
   }
-}
-
-const showMuro = () => {
-  document.getElementById("user-name-sign-up").innerHTML = user.name;
-  sectionSignUpUsers.hidden = true;
-  sectionResponseSignUp.hidden = false;
-  sectionLogOut.hidden = false;
-
 }
 
 const signUpByUsers = () => {
@@ -167,11 +244,11 @@ const signUpByUsers = () => {
         console.log("se envió correo de verificación de cuenta al correo");
         showMuro();
       }).catch(function (error) {
-        console.log(error);
+        alert(error);
       });
     }
   });
-  promise.catch(e => console.log(e.message));
+  promise.catch(e => alert(e.message));
 }
 
 //validaciones de signup divisiones para users
@@ -179,39 +256,30 @@ const ableSignUpByUsers = () => {
   let name, email, password, confirmPassword;
   //nombre
   if (txtUserNameSignUp.value.length > 0) {
-    helperNameUserSignUp.hidden = true;
     name = true;
   } else if (!txtUserNameSignUp.value.length > 0) {
-    helperNameUserSignUp.hidden = false;
     name = false;
   }
   //email
   if (txtUserEmailSignUp.value.length > 0 && patronEmail.test(txtUserEmailSignUp.value)) {
-    helperEmailUserSignUp.hidden = true;
     email = true;
   } else if (txtUserEmailSignUp.value.length === 0 || !patronEmail.test(txtUserEmailSignUp.value)) {
-    helperEmailUserSignUp.hidden = false;
     email = false;
   }
   //new password
   if (txtUserPasswordSignUp.value.length >= 6) {
-    helperPasswordUserSignUp.hidden = true;
     password = true;
   } else if (txtUserPasswordSignUp.value.length < 6) {
-    helperPasswordUserSignUp.hidden = false;
     password = false;
   }
   //confirm password
   if (txtUserConfirmPasswordSignUp.value.length >= 6 && txtUserConfirmPasswordSignUp.value === txtUserPasswordSignUp.value) {
-    helperConfirmPasswordUserSignUp.hidden = true;
     confirmPassword = true;
   } else if (txtUserConfirmPasswordSignUp.value.length < 6 || txtUserConfirmPasswordSignUp.value !== txtUserPasswordSignUp.value) {
-    helperConfirmPasswordUserSignUp.hidden = false;
     confirmPassword = false;
   }
   // si todas las etiquetas estan ocultas hará el registro
   if (name && email && password && confirmPassword) {
-    console.log("todo ok");
     user.name = txtUserNameSignUp.value;
     user.email = txtUserEmailSignUp.value;
     user.password = txtUserPasswordSignUp.value;
@@ -243,10 +311,8 @@ const googleAccount = () => {
     const person = result.user;
     document.getElementById("user-name-log-in").innerHTML = `Bienvenido a Salutem ${person.displayName}`;
   }).catch(function (error) {
-    console.log(error.code);
-    console.log(error.message);
-    console.log(error.email);
-    console.log(error.credential);
+    // console.log(error.code);
+    // console.log(error.message);
   });
 }
 
@@ -257,14 +323,13 @@ const facebookAccount = () => {
     const person = result.user;
     document.getElementById("user-name-log-in").innerHTML = `Bienvenido a Salutem ${person.displayName}`;
   }).catch(function (error) {
-    console.log(error.code);
-    console.log(error.message);
-    console.log(error.email);
-    console.log(error.credential);
+    // console.log(error.code);
+    // console.log(error.message);
   });
 }
 
 let openModal = (e) => {
+  M.updateTextFields();
   if (e.currentTarget.id === "sign-up-selection-users") {
     modalUsers.style.display = "block";
   } else {
@@ -277,28 +342,118 @@ let closeModel = (e) => {
     modalUsers.style.display = "none";
   } else {
     modalDoctors.style.display = "none";
+    txtEspecialidad.value = "";
+    txtColegiatura.value = "";
   }
 };
 
 btnLogIn.addEventListener("click", () => validateLogIn());
 goToSignUp.addEventListener("click", () => showSignUp());
-goToSignUpUsers.addEventListener("click", (e) => openModal(e));
 goToSignUpDoctors.addEventListener("click", (e) => openModal(e));
-closeModalUsers.addEventListener("click", (e) => closeModel(e));
+goToSignUpUsers.addEventListener("click", (e) => openModal(e));
 closeModalDoctors.addEventListener("click", (e) => closeModel(e));
-btnModalSignUpUsers.addEventListener("click", (e) => signUpUsers(e));
-btnModalSignUpDoctors.addEventListener("click", (e) => signUpUsers(e));
+closeModalUsers.addEventListener("click", (e) => closeModel(e));
 goToLogInFromDoctors.addEventListener("click", () => showLogIn());
 goToLogInFromUsers.addEventListener("click", () => showLogIn());
-// btnSignUpDoctors.addEventListener("click", () => signUp());
+btnSignUpDoctors.addEventListener("click", () => ableSignUpByDoctors());
 btnSignUpUsers.addEventListener("click", () => ableSignUpByUsers()); //signUpByUsers
 btnGoogleLogIn.addEventListener("click", () => googleAccount());
 btnFacebookLogIn.addEventListener("click", () => facebookAccount());
 btnLogOut.addEventListener("click", () => logOut());
+
+//button modal
+btnModalSignUpDoctors.addEventListener("click", (e) => {
+  if (txtColegiatura.value.length > 0 && txtEspecialidad.value.length) {
+    helperColegiatura.hidden = true;
+    helperEspecialidad.hidden = true;
+    signUpUsers(e)
+  } else {
+    helperColegiatura.hidden = false;
+    helperEspecialidad.hidden = false;
+  }
+});
+btnModalFbSignUpDoctors.addEventListener("click", () => {
+  if (txtColegiatura.value.length > 0 && txtEspecialidad.value.length) {
+    helperColegiatura.hidden = true;
+    helperEspecialidad.hidden = true;
+    facebookAccount();
+  } else {
+    helperColegiatura.hidden = false;
+    helperEspecialidad.hidden = false;
+  }
+});
+btnModalGgSignUpDoctors.addEventListener("click", () => {
+  if (txtColegiatura.value.length > 0 && txtEspecialidad.value.length) {
+    helperColegiatura.hidden = true;
+    helperEspecialidad.hidden = true;
+    googleAccount();
+  } else {
+    helperColegiatura.hidden = false;
+    helperEspecialidad.hidden = false;
+  }
+});
+btnModalSignUpUsers.addEventListener("click", (e) => signUpUsers(e));
 btnModalFbSignUpUsers.addEventListener("click", () => facebookAccount());
 btnModalGgSignUpUsers.addEventListener("click", () => googleAccount());
-btnModalFbSignUpDoctors.addEventListener("click", () => facebookAccount());
-btnModalGgSignUpDoctors.addEventListener("click", () => googleAccount());
 
-// TODO:
-// al seleccionar el tipo de usuario aparece un modal que muestra botones, en gg y fb hace el registro pero no desaparece la vista de selccion y aparece el muro
+txtDoctorNameSignUp.addEventListener("keyup", () => {
+  if (txtDoctorNameSignUp.value.length > 0) {
+    helperNameDoctorSignUp.hidden = true;
+  } else if (!txtDoctorNameSignUp.value.length > 0) {
+    helperNameDoctorSignUp.hidden = false;
+  }
+});
+txtDoctorEmailSignUp.addEventListener("keyup", () => {
+  if (txtDoctorEmailSignUp.value.length > 0 && patronEmail.test(txtDoctorEmailSignUp.value)) {
+    helperEmailDoctorSignUp.hidden = true;
+  } else if (txtDoctorEmailSignUp.value.length === 0 || !patronEmail.test(txtDoctorEmailSignUp.value)) {
+    helperEmailDoctorSignUp.hidden = false;
+  }
+});
+txtDoctorPasswordSignUp.addEventListener("keyup", () => {
+  if (txtDoctorPasswordSignUp.value.length >= 6) {
+    helperPasswordDoctorSignUp.hidden = true;
+    password = true;
+  } else if (txtDoctorPasswordSignUp.value.length < 6) {
+    helperPasswordDoctorSignUp.hidden = false;
+    password = false;
+  }
+});
+txtDoctorConfirmPasswordSignUp.addEventListener("keyup", () => {
+  if (txtDoctorConfirmPasswordSignUp.value.length >= 6 && txtDoctorConfirmPasswordSignUp.value === txtDoctorPasswordSignUp.value) {
+    helperConfirmPasswordDoctorSignUp.hidden = true;
+    confirmPassword = true;
+  } else if (txtDoctorConfirmPasswordSignUp.value.length < 6 || txtDoctorConfirmPasswordSignUp.value !== txtDoctorPasswordSignUp.value) {
+    helperConfirmPasswordDoctorSignUp.hidden = false;
+    confirmPassword = false;
+  }
+});
+
+txtUserNameSignUp.addEventListener("keyup", () => {
+  if (txtUserNameSignUp.value.length > 0) {
+    helperNameUserSignUp.hidden = true;
+  } else if (!txtUserNameSignUp.value.length > 0) {
+    helperNameUserSignUp.hidden = false;
+  }
+});
+txtUserEmailSignUp.addEventListener("keyup", () => {
+  if (txtUserEmailSignUp.value.length > 0 && patronEmail.test(txtUserEmailSignUp.value)) {
+    helperEmailUserSignUp.hidden = true;
+  } else if (txtUserEmailSignUp.value.length === 0 || !patronEmail.test(txtUserEmailSignUp.value)) {
+    helperEmailUserSignUp.hidden = false;
+  }
+});
+txtUserPasswordSignUp.addEventListener("keyup", () => {
+  if (txtUserPasswordSignUp.value.length >= 6) {
+    helperPasswordUserSignUp.hidden = true;
+  } else if (txtUserPasswordSignUp.value.length < 6) {
+    helperPasswordUserSignUp.hidden = false;
+  }
+});
+txtUserConfirmPasswordSignUp.addEventListener("keyup", () => {
+  if (txtUserConfirmPasswordSignUp.value.length >= 6 && txtUserConfirmPasswordSignUp.value === txtUserPasswordSignUp.value) {
+    helperConfirmPasswordUserSignUp.hidden = true;
+  } else if (txtUserConfirmPasswordSignUp.value.length < 6 || txtUserConfirmPasswordSignUp.value !== txtUserPasswordSignUp.value) {
+    helperConfirmPasswordUserSignUp.hidden = false;
+  }
+});
