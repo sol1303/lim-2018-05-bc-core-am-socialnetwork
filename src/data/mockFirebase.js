2be4b9570b7050758871713ccc6052bb8f72d10c
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-07-24 11:18:23 -0500

    validator

--- src/data/mockFirebase.js
+++ src/data/mockFirebase.js
@@ -1,3 +1,17 @@
+const firebase = require("firebase");
+
+const config = {
+  apiKey: "AIzaSyADhe8BrL2a1vVRQnECNe4np96pxkwgoSw",
+  authDomain: "salutem-a2461.firebaseapp.com",
+  databaseURL: "https://salutem-a2461.firebaseio.com",
+  projectId: "salutem-a2461",
+  storageBucket: "salutem-a2461.appspot.com",
+  messagingSenderId: "953244358481"
+};
+
+firebase.initializeApp(config);
+
+
 window.logIn = (email, password) => {
   const auth = firebase.auth();
   const promise = auth.signInWithEmailAndPassword(email, password);
--- test/mockFirebase.spec.js
+++ test/mockFirebase.spec.js
@@ -1,8 +1,50 @@
+var firebasemock = require('firebase-mock');
+
+var mockauth = new firebasemock.MockAuthentication();
+var mockdatabase = new firebasemock.MockFirebase();
+var mockfirestore = new firebasemock.MockFirestore();
+var mockstorage = new firebasemock.MockStorage();
+var mockmessaging = new firebasemock.MockMessaging();
+var mocksdk = new firebasemock.MockFirebaseSdk(
+  // use null if your code does not use RTDB
+  (path) => {
+    return path ? mockdatabase.child(path) : mockdatabase;
+  },
+  // use null if your code does not use AUTHENTICATION
+  () => {
+    return mockauth;
+  },
+  // use null if your code does not use FIRESTORE
+  () => {
+    return mockfirestore;
+  },
+  // use null if your code does not use STORAGE
+  () => {
+    return mockstorage;
+  },
+  // use null if your code does not use MESSAGING
+  () => {
+    return mockmessaging;
+  }
+);
+
 describe("mockFirebase", () => {
 
   // solo la login
   it("la función de logIn debe ser global", () => {
     assert.isFunction(logIn);
+    mocksdk.auth().autoFlush();
+
+  // create user
+    mocksdk.auth().createUser({
+      uid: '123',
+      email: 'test@test.com',
+      password: 'abc123'
+    }).then(function(user) {
+      // set user as current user for client logic
+      mocksdk.auth().changeAuthState(user);
+    });
+    logIn("test@test.com", "abc123")
   });
 
   // solo facebook
